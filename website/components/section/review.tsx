c7efc64c0cf238e0bb53952a8757d5055f6f7ea0
ValentinoTriadi <valent122016@gmail.com>
2024-06-29 13:09:46 +0700

    fix: fix review button section padding and size

--- website/components/section/review.tsx
+++ website/components/section/review.tsx
@@ -6,7 +6,7 @@ export const Review = () => {
     <div className="w-full gap-1 lg:p-28 p-10 pl-15 lg:pl-52 flex flex-col items-start justify-center" id="review">
       <h1 className="text-4xl md:text-7xl font-bold text-start text-primary-foreground">Your feedback matters!</h1>
       <h2 className="text-xl md:text-3xl font-bold text-start text-accent-foreground">Share your thoughts about our service.</h2>
-      <Button className="text-xl font-semibold p-5 md:mt-16 mt-8"><Link href="/review">Review</Link></Button>
+      <Button className="text-md font-semibold p-5 md:mt-12 mt-8"><Link href="/review">Review</Link></Button>
     </div>
   )
 }
